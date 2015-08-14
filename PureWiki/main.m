0552345d740aad577155e030a2f4c1ab92f15cf0
Tong Kuo <tong-kuo@outlook.com>
2015-08-14 15:36:35 +0800

    Initial commit

---
+++ PureWiki/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  PureWiki
+//
+//  Created by Tong G. on 8/14/15.
+//  Copyright © 2015 TongKuo. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
