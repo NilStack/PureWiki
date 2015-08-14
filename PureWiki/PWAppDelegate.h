e5cd2256b8fc7d180f29c1130b0656cadddb979b
Tong Kuo <tong-kuo@outlook.com>
2015-08-14 15:58:12 +0800

    Okay

---
+++ PureWiki/PWAppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  PWAppDelegate.h
+//  PureWiki
+//
+//  Created by Tong G. on 8/14/15.
+//  Copyright © 2015 TongKuo. All rights reserved.
+//
+
+@import Cocoa;
+
+// PWAppDelegate class
+@interface PWAppDelegate : NSObject <NSApplicationDelegate>
+
+@end // PWAppDelegate class
---
+++ PureWiki/PWAppDelegate.m
@@ -0,0 +1,21 @@
+//
+//  PWAppDelegate.m
+//  PureWiki
+//
+//  Created by Tong G. on 8/14/15.
+//  Copyright © 2015 TongKuo. All rights reserved.
+//
+
+#import "PWAppDelegate.h"
+
+// Private Interfaces
+@interface PWAppDelegate ()
+
+@property ( weak ) IBOutlet NSWindow* window;
+
+@end // Private Interfaces
+
+// PWAppDelegate class
+@implementation PWAppDelegate
+
+@end // PWAppDelegate class
