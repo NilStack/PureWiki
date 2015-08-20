48b6b7a81696e1b1a5d713fadb85a094a4f58241
Tong Kuo <tong-kuo@outlook.com>
2015-08-20 23:25:49 +0800

    Trying to implement the PWSmartSearchBar

--- PureWiki/PWMainWindow.m
+++ PureWiki/PWMainWindow.m
@@ -23,12 +23,44 @@
   ██████████████████████████████████████████████████████████████████████████████*/
 
 #import "PWMainWindow.h"
+#import <objc/message.h>
 
 @implementation PWMainWindow
+
+- ( id ) performSelector: ( SEL )_Selector
+                 asClass: ( Class )_Class
+    {
+    struct objc_super newSuper =
+        { .receiver = self
+        , .super_class = class_isMetaClass( object_getClass( self ) )   // check if we are an instance or Class
+                            ? object_getClass( _Class )                 // if we are a Class, we need to send our metaclass (our Class's Class)
+                            : _Class                                    // if we are an instance, we need to send our Class (which we already have)
+        };
+
+    id ( *objc_superAllocTyped )( struct objc_super*, SEL ) = ( void* )&objc_msgSendSuper;  // cast our pointer so the compiler can sort out the ABI
+    return ( *objc_superAllocTyped )( &newSuper, _Selector );
+    }
 
 - ( void ) awakeFromNib
     {
     self.titleVisibility = NSWindowTitleHidden;
+
+    NSImage* image = [ self performSelector: @selector( _cornerMask ) asClass: self.superclass ];
+    NSLog( @"%@", image );
+
+    image = [ self performSelector: @selector( _cornerMask ) asClass: self.superclass ];
+    NSLog( @"%@", image );
+
+    NSImage* image1 = [ self performSelector: @selector( _cornerMask ) asClass: self.class ];
+    NSLog( @"%@", image1 );
+    }
+
+- ( NSImage* ) _cornerMask
+    {
+    NSImage* image1 = [ self performSelector: @selector( _cornerMask ) asClass: self.superclass ];
+    NSLog( @"🐒%@", image1 );
+
+    return nil;
     }
 
 @end
