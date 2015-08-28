f36a4c82e97244d5a81a82479016fa6c8e2f4f05
Tong Kuo <tong-kuo@outlook.com>
2015-08-28 17:55:31 +0800

    Introduced TKSafariSearchbar

---
+++ PureWiki/TKSafariSearchbar.m
@@ -0,0 +1,56 @@
+/*=============================================================================┐
+|            _______ _     _       ______          ___             _           |  
+|           (_______|_)   | |     / _____)        / __)           (_)          |██
+|               _    _____| |____( (____  _____ _| |__ _____  ____ _           |██
+|              | |  |  _   _|_____)____ \(____ (_   __|____ |/ ___) |          |██
+|              | |  | |  \ \      _____) ) ___ | | |  / ___ | |   | |          |██
+|              |_|  |_|   \_)    (______/\_____| |_|  \_____|_|   |_|          |██
+|                                                                              |██
+|             ______                        _     _                            |██
+|            / _____)                      | |   | |                           |██
+|           ( (____  _____ _____  ____ ____| |__ | |__  _____  ____            |██
+|            \____ \| ___ (____ |/ ___) ___)  _ \|  _ \(____ |/ ___)           |██
+|            _____) ) ____/ ___ | |  ( (___| | | | |_) ) ___ | |               |██
+|           (______/|_____)_____|_|   \____)_| |_|____/\_____|_|               |██
+|                                                                              |██
+|                                                                              |██
+|                         Copyright (c) 2015 Tong Kuo                          |██
+|                                                                              |██
+|                             ALL RIGHTS RESERVED.                             |██
+|                                                                              |██
+└==============================================================================┘██
+  ████████████████████████████████████████████████████████████████████████████████
+  ██████████████████████████████████████████████████████████████████████████████*/
+
+#import "TKSafariSearchbar.h"
+
+// TKSafariSearchbar class
+@implementation TKSafariSearchbar
+
+@end
+
+/*===============================================================================┐
+|                                                                                |
+|                           The MIT License (MIT)                                |
+|                                                                                |
+|                        Copyright (c) 2015 Tong Kuo                             |
+|                                                                                |
+| Permission is hereby granted, free of charge, to any person obtaining a copy   |
+| of this software and associated documentation files (the "Software"), to deal  |
+| in the Software without restriction, including without limitation the rights   |
+| to use, copy, modify, merge, publish, distribute, sublicense, and/or sell      |
+|   copies of the Software, and to permit persons to whom the Software is        |
+|         furnished to do so, subject to the following conditions:               |
+|                                                                                |
+| The above copyright notice and this permission notice shall be included in all |
+|              copies or substantial portions of the Software.                   |
+|                                                                                |
+| THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR     |
+| IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,       |
+| FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE    |
+|  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER        |
+| LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,  |
+| OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE  |
+|                                 SOFTWARE.                                      |
+|                                                                                |
+└===============================================================================*/
