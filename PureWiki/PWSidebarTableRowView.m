504b13782d0135f43277445528c86ea1968fbae3
Tong Kuo <contact@tong-kuo.me>
2015-09-23 15:26:39 +0800

    Introduced a custom table row view for sidebar

--- PureWiki/PWSidebarTableRowView.m
+++ PureWiki/PWSidebarTableRowView.m
@@ -22,126 +22,33 @@
   ████████████████████████████████████████████████████████████████████████████████
   ██████████████████████████████████████████████████████████████████████████████*/
 
-#import "PWSidebarTabsTableController.h"
-#import "PWSidebarTabsTable.h"
-#import "PWSidebarTabsTableCell.h"
-#import "PWActionNotifications.h"
-#import "PWActionNotifications.h"
-#import "PWWikiContentView.h"
-#import "PWOpenedWikiPage.h"
 #import "PWSidebarTableRowView.h"
+#import "NSColor+TKSafariSearchbar.h"
 
-#import "SugarWiki.h"
+// PWSidebarTableRowView class
+@implementation PWSidebarTableRowView
 
-NSString* const PWSidebarCurrentSelectedPageKVOPath = @"currentSelectedPage";
-
-NSString* const kColumnIdentifierTabs = @"tabs-column";
-
-// Private Interfaces
-@interface PWSidebarTabsTableController ()
-@end // Private Interfaces
-
-// PWSidebarTabsTableController class
-@implementation PWSidebarTabsTableController
-
-@dynamic currentSelectedPage;
-
-- ( instancetype ) initWithCoder: ( nonnull NSCoder* )_Coder
+- ( void ) drawSelectionInRect: ( NSRect )_DirtyRect
     {
-    if ( self = [ super initWithCoder: _Coder ] )
-        self->_openedWikiPages = [ NSMutableArray array ];
-
-    return self;
-    }
-
-#pragma mark Handling Data Source
-- ( void ) pushOpenedWikiPage: ( PWOpenedWikiPage* )_OpendedWikiPage
-    {
-    if ( _OpendedWikiPage )
+    if ( self.selectionHighlightStyle == NSTableViewSelectionHighlightStyleRegular )
         {
-        NSUInteger index = [ self->_openedWikiPages indexOfObject: _OpendedWikiPage ];
-        BOOL isPushing = NO;
+        NSColor* beautyColor = nil;
 
-        if ( index == NSNotFound )
-            {
-            [ self->_openedWikiPages addObject: _OpendedWikiPage ];
-            isPushing = YES;
-            }
+        if ( self.isSelected )
+            beautyColor = [ [ NSColor colorWithHTMLColor: @"F5A623" ] colorWithAlphaComponent: .65f ];
         else
-            {
-            PWOpenedWikiPage* opendedWikiPage = self->_openedWikiPages[ index ];
+            beautyColor = [ NSColor clearColor ];
 
-            if ( opendedWikiPage.openedWikiPage != _OpendedWikiPage.openedWikiPage )
-                opendedWikiPage.openedWikiPage = _OpendedWikiPage.openedWikiPage;
-            }
+        [ beautyColor setStroke ];
+        [ beautyColor setFill ];
 
-        [ self.sidebarTabsTable reloadData ];
-
-        NSIndexSet* selectRowIndexes =
-            [ NSIndexSet indexSetWithIndex: isPushing ? ( self->_openedWikiPages.count - 1 )
-                                                      : [ self->_openedWikiPages indexOfObject: self->_currentSelectedPage ] ];
-
-        [ self.sidebarTabsTable selectRowIndexes: selectRowIndexes byExtendingSelection: NO ];
+        NSBezierPath* selectionPath = [ NSBezierPath bezierPathWithRect: _DirtyRect ];
+        [ selectionPath fill ];
+        [ selectionPath stroke ];
         }
     }
 
-#pragma mark Conforms to <NSTableViewDataSource>
-- ( NSInteger ) numberOfRowsInTableView: ( nonnull NSTableView* )_TableView
-    {
-    return self->_openedWikiPages.count;
-    }
-
-- ( id )            tableView: ( nonnull NSTableView* )_TableView
-    objectValueForTableColumn: ( nullable NSTableColumn* )_TableColumn
-                          row: ( NSInteger )_Row
-    {
-    id result = nil;
-
-    if ( [ _TableColumn.identifier isEqualToString: kColumnIdentifierTabs ] )
-        result = self->_openedWikiPages[ _Row ];
-
-    return result;
-    }
-
-#pragma mark Conforms to <NSTableViewDelegate>
-- ( NSView* ) tableView: ( nonnull NSTableView* )_TableView
-     viewForTableColumn: ( nullable NSTableColumn* )_TableColumn
-                    row: ( NSInteger )_Row
-    {
-    PWSidebarTabsTableCell* resultCellView = [ _TableView makeViewWithIdentifier: _TableColumn.identifier owner: self ];
-    PWOpenedWikiPage* opendedWikiPage = [ _TableView.dataSource tableView: _TableView objectValueForTableColumn: _TableColumn row: _Row ];
-    [ resultCellView setOpenedWikiPage: opendedWikiPage ];
-
-    return resultCellView;
-    }
-
-- ( void ) tableViewSelectionDidChange: ( nonnull NSNotification* )_Notification
-    {
-    NSInteger selectedRowIndex = self.sidebarTabsTable.selectedRow;
-    
-    [ self willChangeValueForKey: PWSidebarCurrentSelectedPageKVOPath ];
-        self->_currentSelectedPage = self->_openedWikiPages[ selectedRowIndex ];
-    [ self didChangeValueForKey: PWSidebarCurrentSelectedPageKVOPath ];
-    }
-
-- ( NSTableRowView* ) tableView: ( NSTableView* )_TableView
-                  rowViewForRow: ( NSInteger )_Row
-    {
-    PWSidebarTableRowView* rowView = [ _TableView rowViewAtRow: _Row makeIfNecessary: YES ];
-
-    if ( !rowView )
-        rowView = [ [ PWSidebarTableRowView alloc ] initWithFrame: NSZeroRect ];
-
-    return rowView;
-    }
-
-#pragma mark Dynamic Properties
-- ( PWOpenedWikiPage* ) currentSelectedPage
-    {
-    return self->_currentSelectedPage;
-    }
-
-@end // PWSidebarTabsTableController class
+@end // PWSidebarTableRowView class
 
 /*===============================================================================┐
 |                                                                                | 
